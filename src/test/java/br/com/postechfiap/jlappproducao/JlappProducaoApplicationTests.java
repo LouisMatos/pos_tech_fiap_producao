bb5b1aa06e82401bfd03f7fb7e47ded0e1015fd2
Luis Matos <luis.felipe.pereira.matos@gmail.com>
2024-05-05 10:38:47 -0300

    first commit

---
+++ src/test/java/br/com/postechfiap/jlappproducao/JlappProducaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.postechfiap.jlappproducao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JlappProducaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
